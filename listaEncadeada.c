5417e114768e5bb46e017922cd5d30e6c975b2a2
Gabriel Leoncio Verdi <101580365+glVerdi@users.noreply.github.com>
2024-04-23 10:59:15 -0300

    lista encadeada com alocação dinamica

---
+++ listaEncadeada.c
@@ -0,0 +1,10 @@
+typedef struct {
+    Node* head;
+    Node* tail;
+    int size;   // conveniente sempre saber o tamanho
+} List;
+
+len(List lst)
+add(List* lst, int value)
+dump(List lst)
+create(List* lst) // List* create ()
