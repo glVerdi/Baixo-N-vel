13b543a61f12cf520c1a922b2b619d03707f8ae3
Gabriel Leoncio Verdi <101580365+glVerdi@users.noreply.github.com>
2024-04-25 11:08:00 -0300

    Incrementando

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -1,10 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Estrutura de um nó
+typedef struct Node {
+    int data;
+    struct TNode* next;   // conveniente sempre saber o tamanho
+} Node;
+
 typedef struct {
     Node* head;
     Node* tail;
     int size;   // conveniente sempre saber o tamanho
 } List;
 
-len(List lst)
-add(List* lst, int value)
-dump(List lst)
-create(List* lst) // List* create ()
+//len(List* lst): retorna o comprimento da lista
+int len(List* lst);
+//add(List* lst, int value): adiciona um elemento ao início da lista
+void add(List* lst, int value);
+//dump(List* lst): mostra o conteúdo da lista
+void dump(List* lst);
+//Lst* create(): cria um descritor para lista
+List* create();
+
+int len(List* lst) {
+    return (*lst).size;
+}
+
+int main(void){
+    printf("Listas!\n");
+    List lst1;
+    lst1.size = 42;
+    printf("%d\n", lst1.size);
+    int s = len(&lst1);
+    printf("%d\n", s);
+    return EXIT_SUCCESS;
+}
+                                                                                                                                    26,10         Top
